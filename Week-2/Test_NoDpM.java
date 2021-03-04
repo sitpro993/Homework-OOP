1b134dbec743d83de68d1dcd8070aeb1af5839b6
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2021-03-04 10:14:49 +0700

    them bt week 2

--- Week-2/Test_NoDpM.java
+++ Week-2/Test_NoDpM.java
@@ -2,27 +2,40 @@ import java.util.Scanner;
 
 public class Test_NoDpM {
 
-public static void main(String[] args) {
-    NumberOfDaysPerMonth d = new NumberOfDaysPerMonth();
-    String month = null;
-    int m,y;
+    public static void main(String[] args) {
+        NumberOfDaysPerMonth d = new NumberOfDaysPerMonth();
+        String month = null;
+        String check;
+        String s1 = "Y";
+        String s2 = "N";
+        int m, y;
 
+        while (true) {
+            while (true) {
+                System.out.printf("Month: ");
+                month = new Scanner(System.in).nextLine();
+                m = d.input_month(month);
+                if (m > 0) break;
+            }
+            while (true) {
+                System.out.printf("Year: ");
+                y = new Scanner(System.in).nextInt();
+                if (d.check_year(y) == true) {
+                    break;
+                } else System.out.println("Invalid inputs. Please enter again.");
+            }
+            d = new NumberOfDaysPerMonth(m, y);
+            d.resolve(m, y);
+            while (true) {
+                System.out.printf("You want to continue(Y/N): ");
+                check = new Scanner(System.in).nextLine();
+                if(check.compareTo(s1) == 0 || check.compareTo(s2) == 0) break;
+                else System.out.println("Invalid inputs----. Please enter again.");
+            }
+            if(check.compareTo(s1) == 0) continue;
+            if (check.compareTo(s2) == 0) break;
 
-    while (true){
-        System.out.printf("Month: ");
-        month = new Scanner(System.in).nextLine();
-        m = d.input_month(month);
-        if(m > 0) break;
-    }
-    while (true) {
-        System.out.printf("Year: ");
-        y = new Scanner(System.in).nextInt();
-        if (d.check_year(y) == true) {
-            break;
-        } else System.out.println("Invalid inputs. Please enter again.");
-    }
-    d = new NumberOfDaysPerMonth(m,y);
-    d.resolve(m,y);
+        }
     }
 
 }
