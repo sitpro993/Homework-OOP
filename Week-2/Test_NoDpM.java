46f0599786f26024e08d0cf5c4a403f1ac447c13
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2021-03-04 10:25:37 +0700

    them bt week 2

--- Week-2/Test_NoDpM.java
+++ Week-2/Test_NoDpM.java
@@ -4,28 +4,28 @@ public class Test_NoDpM {
 
     public static void main(String[] args) {
         NumberOfDaysPerMonth d = new NumberOfDaysPerMonth();
-        String month = null;
+        String month;
         String check;
         int m, y;
 
         while (true) {
             while (true) {
-                System.out.printf("Month: ");
+                System.out.print("Month: ");
                 month = new Scanner(System.in).nextLine();
                 m = d.input_month(month);
                 if (m > 0) break;
             }
             while (true) {
-                System.out.printf("Year: ");
+                System.out.print("Year: ");
                 y = new Scanner(System.in).nextInt();
-                if (d.check_year(y) == true) {
+                if (d.check_year(y)) {
                     break;
                 } else System.out.println("Invalid inputs. Please enter again.");
             }
             d = new NumberOfDaysPerMonth(m, y);
             d.resolve(m, y);
             while (true) {
-                System.out.printf("You want to continue(Y/N): ");
+                System.out.print("You want to continue(Y/N): ");
                 check = new Scanner(System.in).nextLine();
                 if(check.compareTo("Y") == 0 || check.compareTo("N") == 0) break;
                 else System.out.println("Invalid inputs----. Please enter again.");
@@ -35,3 +35,5 @@ public class Test_NoDpM {
 
         }
     }
+
+}
