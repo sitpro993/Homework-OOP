1c8004696ccc0bc01b8811be1b62e44eb09d7bc0
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2021-03-30 16:17:51 +0700

    sua bt tuan 5

--- HEDSPI/OtherProjects/src/hust/soict/hedspi/garbage/GarbageCreator.java
+++ HEDSPI/OtherProjects/src/hust/soict/hedspi/garbage/GarbageCreator.java
@@ -1,4 +1,20 @@
 package hust.soict.hedspi.garbage;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class GarbageCreator {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("C:\\Users\\sitpr\\IdeaProjects\\HEDSPI\\OtherProjects\\src\\hust\\soict\\hedspi\\garbage\\text");
+        Scanner sc = new Scanner(fr);
+        String line = "";
+        while(sc.hasNext() == true){
+            line = sc.nextLine();
+            line += sc.nextLine();
+        }
+        System.out.println(line);
+        // Close the file.
+        sc.close();
+    }
 }
--- HEDSPI/OtherProjects/src/hust/soict/hedspi/garbage/NoGarbage.java
+++ HEDSPI/OtherProjects/src/hust/soict/hedspi/garbage/NoGarbage.java
@@ -1,4 +1,20 @@
 package hust.soict.hedspi.garbage;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class NoGarbage {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("C:\\Users\\sitpr\\IdeaProjects\\HEDSPI\\OtherProjects\\src\\hust\\soict\\hedspi\\garbage\\text");
+        Scanner sc = new Scanner(fr);
+        StringBuffer line = new StringBuffer();
+        while(sc.hasNext()){
+            line.append(sc.nextLine());
+            line.append("\n");
+        }
+        System.out.println(line);
+        // Close the file.
+        sc.close();
+    }
 }
